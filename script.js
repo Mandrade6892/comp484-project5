bb36589dffc15a439c13085d786eea3cf35342d0
Mandrade6892 <2002andradejr@gmail.com>
2026-05-14 12:11:43 -0700

    Add location data for Addie Klotz Student Health Center in Map Quiz

--- script.js
+++ script.js
@@ -17,6 +17,16 @@ const locations = [
       west: -118.5310
     }
   }
+  {
+  name: "Addie Klotz Student Health Center",
+  grid: "F3",
+  bounds: {
+    north: 34.2407,
+    south: 34.2399,
+    east: -118.5253,
+    west: -118.5264
+  }
+}
 ];
 let currentQuestion = 0;
 let correct = 0;
